339d7fa17dfed217d165aca46bf6552c9159a449
Raghav <raghav.kaashyap@gmail.com>
2025-01-29 19:16:01 -0600

    Minor change

--- src/components/ResumeModal.jsx
+++ src/components/ResumeModal.jsx
@@ -12,7 +12,7 @@ const ResumeModal = ({ onClose }) => {
                 </button>
                 <h2 className="text-2xl font-bold mb-4 text-center">My Resume</h2>
                 <iframe
-                    src="/assets/Raghav_Kaashyap_resume.pdf"
+                    src="/assets/Raghav_Portfolio_Resume.pdf"
                     className="w-full h-96"
                     title="Resume"
                 />
